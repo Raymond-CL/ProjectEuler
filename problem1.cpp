6d285c8640061aafd3a06b3c04ff9b7a4644b95a
Raymond-CL <raymond.chen88@outlook.com>
2020-04-14 15:37:18 +0800

    problem1 updated

--- problem1.cpp
+++ problem1.cpp
@@ -8,11 +8,13 @@ void problem1(){
     int sum=0;
 
     /*
-    for(int i=1;i<max;i++){
+	// brute force
+    for(int i=1;i<max;i++)
 		if(i%3==0 || i%5==0)
 			sum+=i;
-    }*/
+    */
 
+    // use sum properties
     sum += (3+999)*(999/3)/2;
     sum += (5+995)*(999/5)/2;
     sum -= (15+990)*(999/15)/2;
