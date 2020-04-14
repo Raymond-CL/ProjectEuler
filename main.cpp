8d52851900fa364edbf0acd5229d560800b56f58
Raymond-CL <raymond.chen88@outlook.com>
2020-04-14 16:18:53 +0800

    problem10 updated

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 //#include "problem2.cpp"
 //#include "problem3.cpp"
 //#include "problem6.cpp"
-#include "problem7.cpp"
+//#include "problem7.cpp"
+#include "problem10.cpp"
 
 int main(){
 
-	problem7();      
+	problem10();      
 	return 0;
 }
