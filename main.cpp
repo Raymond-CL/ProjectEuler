21c9c3de19c27fb44c990e99f6a91cf8022c6d42
Raymond-CL <raymond.chen88@outlook.com>
2020-04-24 00:16:38 +0800

    problem19 updated

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 //#include "problem15.cpp"
 //#include "problem16.cpp"
 //#include "problem17.cpp"
-#include "problem18.cpp"
-//#include "problem9.cpp"
+//#include "problem18.cpp"
+#include "problem19.cpp"
 //#include "problem10.cpp"
 #include <ctime>
 
@@ -14,7 +14,7 @@ int main(){
 
     std::clock_t t = std::clock();
 
-	problem18();
+	problem19();
 
     t = std::clock() - t;
     cout << "time elapsed: " << 1000.0*t/CLOCKS_PER_SEC << " ms." << endl;
